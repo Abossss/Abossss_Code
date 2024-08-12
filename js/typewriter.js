15c3c1e48dbcf8dc99a4d6149cdd951d23faca7b
Abossss <85421297+Abossss@users.noreply.github.com>
2024-08-12 18:22:37 +0800

    Add files via upload

---
+++ js/typewriter.js
@@ -0,0 +1,14 @@
+var typed = new Typed("#typewriter", {
+    strings: [
+        "先去做，做成一堆烂泥，再慢慢地改",
+        "欲望以提升热忱，毅力以磨平高山",
+        "看似不起波澜的日复一日，一定会在某一天，让你看到坚持的意义。",
+        "坚韧不拔的意志比钻石更硬，破茧成蝶终有时。",
+        "每一个不起舞的日子，都是对生命的辜负。"
+    ],
+    startDelay: 300,
+    typeSpeed: 100,
+    loop: true,
+    backSpeed: 50,
+    showCursor: false
+});
